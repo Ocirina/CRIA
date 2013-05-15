b7b9e27b8286687e3f468b20dc776cc6b952c94f
Willem van Lent <wvlent@deactro.nl>
2013-05-15 11:48:28 +0200

    Done

---
+++ src/main/javascript/Models/Phone.js
@@ -0,0 +1,21 @@
+/**
+ * @author Willem van Lent (willemvanlent@live.nl)
+ * @copyright Copyright (c) 2013, HAN, Arnhem - All rights reserved
+ * @version 1.0.0
+ */
+
+var Phone = (function() {
+    function Phone(){
+
+    };
+
+    Phone.prototype.public = function() {
+
+    };
+
+    var private = function() {
+
+    };
+
+    return Phone;
+})();
