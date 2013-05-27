67b7e66123ae0ff4271c85b8c4eb8af3953cfce7
Brian van Burken <brianburkertje@hotmail.com>
2013-05-27 10:59:06 +0200

    Added DesignObject schema.

---
+++ server/app/models/design_object.js
@@ -0,0 +1,20 @@
+var mongoose, DesignObject, Schema, modelName, collectionName, passwordHash;
+
+mongoose = require('mongoose');
+Schema = mongoose.Schema;
+
+DesignObject = Schema({
+    width:      {type:Number, required:true},
+    height:     {type:Number, required:true},
+    degree:     {type:Number, required:true},
+    xPosition:  {type:Number, required:true},
+    yPosition:  {type:Number, required:true},
+    zIndex:     {type:Number, required:true},
+    image:      {type:Number, required:false},
+    value:      {type:String, required:false}
+    caseDesign: {type:Schema.Type.ObjectID, ref: 'CaseDesign'}
+});
+
+modelName = "DesignObject";
+collectionName = "DesignObjects";
+User = mongoose.model(modelName, Address, collectionName);
