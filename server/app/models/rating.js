a45a8df61333c803449f2eafa19aa4b78d93738f
Brian van Burken <brianburkertje@hotmail.com>
2013-05-27 13:46:43 +0200

    Removed unwanted attributes.

--- server/app/models/rating.js
+++ server/app/models/rating.js
@@ -5,8 +5,6 @@ Schema = mongoose.Schema;
 
 Rating = Schema({
     amount:       {type: Number, required: true},
-    minRating:    {type: Number, required: true},
-    maxRating:    {type: Number, required: true},
     user:         {type: Schema.Types.ObjectId, ref: 'User', required: true},
     caseDesign:   {type: Schema.Types.ObjectId, ref: 'CaseDesign', required: true}
 });
