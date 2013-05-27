89fa56ecf20957fde03741adb60d7eebe368193e
Willem van Lent <wvlent@deactro.nl>
2013-05-27 12:18:05 +0200

    Route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,37 +7,37 @@ module.exports = function (app) {
      */
 
     // User
-    app.get('/user', user.index);
+    app.get('/users', user.index);
+    app.post('/users', user.create);
     app.get('/user/:id', user.show);
-    app.post('/user/new', user.create);
     app.put('/user/:id', user.update);
     app.delete('/user/:id', user.destroy);
 
     // CaseDesign
-    app.get('/casedesign', casedesign.index);
+    app.get('/casedesigns', casedesign.index);    
+    app.post('/casedesigns/new', casedesign.create);
     app.get('/casedesign/:id', casedesign.show);
-    app.post('/casedesign/new', casedesign.create);
     app.put('/casedesign/:id', casedesign.update);
     app.delete('/casedesign/:id', casedesign.destroy);
 
     // Rating
-    app.get('/rating', rating.index);
+    app.get('/ratings', rating.index);
+    app.post('/ratings/new', rating.create);
     app.get('/rating/:id', rating.show);
-    app.post('/rating/new', rating.create);
     app.put('/rating/:id', rating.update);
     app.delete('/rating/:id', rating.destroy);
 
     // Comment
-    app.get('/comment', comment.index);
+    app.get('/comments', comment.index);
+    app.post('/comments/new', comment.create);
     app.get('/comment/:id', comment.show);
-    app.post('/comment/new', comment.create);
     app.put('/comment/:id', comment.update);
     app.delete('/comment/:id', comment.destroy);
 
     // Order
-    app.get('/order', order.index);
+    app.get('/orders', order.index);
+    app.post('/orders/new', order.create);
     app.get('/order/:id', order.show);
-    app.post('/order/new', order.create);
     app.put('/order/:id', order.update);
     app.delete('/order/:id', order.destroy);
 }
