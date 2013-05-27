7be01bc95fa398cccbbd70e9add54cb86ce8667e
Patrick van Vuuren <PMH.vanVuuren@student.han.nl>
2013-05-27 11:11:03 +0200

    fixed rating/comment

---
+++ server/app/models/comment.js
@@ -0,0 +1,14 @@
+var mongoose, Comment, Schema, modelName, collectionName, passwordHash;
+
+mongoose = require('mongoose');
+Schema = mongoose.Schema;
+
+Comment = Schema({
+    text:         {type:String, required: true},
+    user:         {type:Schema.Types.ObjectId, ref: 'User', required:true},
+    caseDesign:   {type:Schema.Types.ObjectId, ref: 'CaseDesign', required:true}
+});
+
+modelName = "Comment";
+collectionName = "comments";
+Comment = mongoose.model(modelName, Comment, collectionName);
--- server/app/models/rating.js
+++ server/app/models/rating.js
@@ -7,8 +7,8 @@ Rating = Schema({
     amount:       {type:Number, required:true},
     minRating:    {type:Number, required:true},
     maxRating:    {type:Number, required:true},
-    user:         {type:Schema.Types.ObjectId, ref: 'User'},
-    caseDesign:   {type:Schema.Types.ObjectId, ref: 'CaseDesign'}
+    user:         {type:Schema.Types.ObjectId, ref: 'User', required:true},
+    caseDesign:   {type:Schema.Types.ObjectId, ref: 'CaseDesign', required:true}
 });
 
 modelName = "Rating";
