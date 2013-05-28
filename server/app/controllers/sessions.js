5a93651dbb63b72261bcc4beded8c2f01c1c1ddd
Brian van Burken <brianburkertje@hotmail.com>
2013-05-28 20:05:36 +0200

    Sessions controller updated to use sessions and validation if online.

--- server/app/controllers/sessions.js
+++ server/app/controllers/sessions.js
@@ -21,6 +21,8 @@ exports.create = function (req, res) {
     .exec(function (err, user) {
       delete user.password;
       // TODO: start session
+      req.session.username = user.name;
+      req.session.lastAccess = new Date().getTime();
       return res.send({
         "error": err,
         "result": user
@@ -29,9 +31,24 @@ exports.create = function (req, res) {
 }
 
 /**
+ * Type: POST
+ * Route: /user/check
+ */
+exports.check = function (req, res) {
+  var result = {"error": "Not signed in!", "result": false};
+  if (req.session.username === res.body.username) {
+    req.session.lastAccess = new Date().getTime();
+    result.error = null;
+    result.result = true;
+  }
+  res.send(result);
+}
+
+/**
  * Type: DELETE
  * Route: /user/signout
  */
 exports.destroy = function (req, res) {
   // TODO: Destroy session.
+  req.session = {}; // Possible?
 }
