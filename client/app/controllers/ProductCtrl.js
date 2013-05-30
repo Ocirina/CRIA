8985bbccb1043441527c034cfe4aa214e6505399
Willem van Lent <willemvanlent@live.nl>
2013-05-30 20:02:39 +0200

    End of the day commit ;-)

--- client/app/controllers/ProductCtrl.js
+++ client/app/controllers/ProductCtrl.js
@@ -1,9 +1,9 @@
-app.controller('ProductController', function($scope, $location, $http, $resource) {
+app.controller('ProductCtrl', function($scope, $location, $http, $resource) {
     $scope.getProducts = function() {
         var Products = $resource('http://autobay.tezzt.nl:43083/casedesigns',{},
-            {charge: {method:'GET', params:{charge:true}}}
+            {charge: {method:'GET'}}
         );
         var products = new Products();
-        console.log(products.get())
+        console.log(products.$get());
     };
 });
