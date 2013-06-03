0993e69000ffbdd1252a1b1a339d49f6fa46fdb0
Willem van Lent <wvlent@deactro.nl>
2013-06-03 20:50:54 +0200

    End of the day commit

--- client/app/controllers/DesignCtrl.js
+++ client/app/controllers/DesignCtrl.js
@@ -1,18 +1,9 @@
 app.controller('DesignCtrl', function($scope, $location) {
     $scope.activatePhone = function(phoneId) {
-        $scope.phone = {
-            iphone5: false,
-            iphone4: false
-        };
-
-        if(phoneId == 5) {
-            $scope.phone.iphone5 = true;
-        } else if(phoneId == 4) {
-            $scope.phone.iphone4 = true;
-        }
+        $scope.phone.phone = phoneId;
     }
 
     $scope.setCaseType = function(caseTypeId) {
-        console.log(caseTypeId);
+        $scope.phone.case = caseTypeId;
     }
 });
