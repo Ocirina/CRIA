3da666397c18318500547d3b0e34e2c78d503c49
Brian van Burken <brianburkertje@hotmail.com>
2013-06-03 11:13:32 +0200

    Revert "Service added like Willem want to :P"
    This reverts commit 52580a918008e18b300ed40ba28afcf587f4cfce.

--- client/app/controllers/ProductCtrl.js
+++ client/app/controllers/ProductCtrl.js
@@ -1,5 +1,18 @@
-app.controller('ProductCtrl', function($scope, casedesigns) {
-    casedesigns.get(function(data){
-         console.log(data);
-    });
+app.controller('ProductCtrl', function($scope, $location, $http, $resource) {
+    $scope.getProducts = function() {
+        var Products = $resource('http://autobay.tezzt.nl\\:43083/casedesigns',{},
+            {charge: {method:'GET', params:{charge:true}}}
+        );
+
+        var product = Products.get();
+
+        console.log(product.$get("result"));
+        console.log(product['g']);
+
+        for(var index in product) {
+            console.warn(product[index], index);
+        }
+
+//        $scope.product.products = products.result;
+    };
 });
