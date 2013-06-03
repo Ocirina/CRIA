0c09490776c35bc0653fdfc046f045b1cb33f9e1
Willem van Lent <wvlent@deactro.nl>
2013-06-03 19:58:58 +0200

    Design track.

--- client/app/controllers/DesignCtrl.js
+++ client/app/controllers/DesignCtrl.js
@@ -1,3 +1,18 @@
 app.controller('DesignCtrl', function($scope, $location) {
+    $scope.activatePhone = function(phoneId) {
+        $scope.phone = {
+            iphone5: false,
+            iphone4: false
+        };
 
+        if(phoneId == 5) {
+            $scope.phone.iphone5 = true;
+        } else if(phoneId == 4) {
+            $scope.phone.iphone4 = true;
+        }
+    }
+
+    $scope.setCaseType = function(caseTypeId) {
+        console.log(caseTypeId);
+    }
 });
