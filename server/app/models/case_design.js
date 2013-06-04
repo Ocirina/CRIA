96c5bedc3bd5f77bae7415311cd9fbfa171a9f28
Brian van Burken <brianburkertje@hotmail.com>
2013-06-04 13:18:02 +0200

    User reference added to casedesign.

--- server/app/models/case_design.js
+++ server/app/models/case_design.js
@@ -6,6 +6,7 @@ var Schema = mongoose.Schema;
 var CaseDesign = Schema({
     name:           {type: String,  required: true, index: true},
     shared:         {type: Boolean, required: true},
+    user:           {type: Schema.Types.ObjectId, ref: 'User'},
     previewImage:   {type: Schema.Types.ObjectId, ref: 'CaseDesign'},
     caseModel:      {type: Schema.Types.ObjectId, ref: 'CaseModel'}
 });
