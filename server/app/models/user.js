ca20b03e91fcb69fa0df0b740442cf663130a30f
Brian van Burken <brianburkertje@hotmail.com>
2013-06-04 12:45:52 +0200

    Address creation added.

--- server/app/models/user.js
+++ server/app/models/user.js
@@ -1,8 +1,10 @@
-var mongoose, User, Schema, modelName, collectionName, passwordHash;
+var mongoose, User, Schema;
 
+// Include dependencies.
 mongoose = require('mongoose');
 Schema = mongoose.Schema;
 
+// Schema definition for User.
 User = Schema({
     name:         {type: String, required: true, index: true, unique: true},
     email:        {type: String, required: true, index: true, unique: true},
@@ -12,6 +14,22 @@ User = Schema({
     password:     {type: String, required: true},
 });
 
-modelName = "User";
-collectionName = "Users";
-User = mongoose.model(modelName, User, collectionName);
+/**
+ * Before saving it checks if there is a address POST-ed with the request. If
+ * this is true then it will save the address to the database.
+ * I've chosen for a middleware function to keep the controllers more clean.
+ * @see http://mongoosejs.com/docs/middleware.html
+ */
+User.pre('save', function(next, req, user, callback){
+  if (req.body['address']) {
+    var Address = mongoose.models["Address"];
+    var item = new Address(req.body.address);
+    item.save(function(err, addr){
+      if (!err) {next(callback);}
+      else {next(err);}
+    });
+  }
+});
+
+// Define the Mongoose model.
+mongoose.model("User", User, "Users");
