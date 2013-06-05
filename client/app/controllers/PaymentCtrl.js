df9fb0a60c357922649e4e6ea9660b9bd6d4b4e4
Patrick van Vuuren <PMH.vanVuuren@student.han.nl>
2013-06-05 13:03:12 +0200

    icons

--- client/app/controllers/PaymentCtrl.js
+++ client/app/controllers/PaymentCtrl.js
@@ -16,28 +16,28 @@ app.controller('PaymentCtrl', function($scope, $location, $http, $resource) {
                 value: 1,
                 selected: false,
                 description: "Betalen met uw eigen bank",
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png"
+                imageSrc: "./img/paymenticons/IDEAL.png"
             },
             {
                 text: "Mastercard",
                 value: 2,
                 selected: false,
                 description: "Betalen met Mastercard",
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/twitter-icon-32.png"
+                imageSrc: "./img/paymenticons/Mastercard.png"
             },
             {
                 text: "Paypal",
                 value: 3,
                 selected: false,
                 description: "Betaal met uw Paypal-account",
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/linkedin-icon-32.png"
+                imageSrc: "./img/paymenticons/Paypal.png"
             },
             {
                 text: "Visa",
                 value: 4,
                 selected: false,
                 description: "Betalen met Visa",
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
+                imageSrc: "./img/paymenticons/Visa.png"
             }
         ];
 
@@ -69,53 +69,46 @@ app.controller('PaymentCtrl', function($scope, $location, $http, $resource) {
                 description: "Kies uw bank"
             },
             {
+                text: "Rabobank",
                 value: 1,
                 selected: false,
-                description: "Rabobank",
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png"
+                imageSrc: "./img/paymenticons/Rabobank.png"
             },
             {
                 text: "ABN Amro",
                 value: 2,
                 selected: false,
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/twitter-icon-32.png"
+                imageSrc: "./img/paymenticons/ABNAMBRO.png"
             },
             {
                 text: "ING",
                 value: 3,
                 selected: false,
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/linkedin-icon-32.png"
+                imageSrc: "./img/paymenticons/ING.png"
             },
             {
                 text: "ASN",
                 value: 4,
                 selected: false,
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
+                imageSrc: "./img/paymenticons/IDEAL.png"
             },
             {
                 text: "SNS Reaal",
                 value: 5,
                 selected: false,
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
+                imageSrc: "./img/paymenticons/Sns.png"
             },
             {
                 text: "Fortis",
                 value: 6,
                 selected: false,
-                description: "Betaal met American Express",
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
+                imageSrc: "./img/paymenticons/Fortis.png"
             },
             {
                 text: "Triodos",
                 value: 7,
                 selected: false,
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
-            },
-            {
-                text: "Friesland Bank",
-                value: 8,
-                selected: false,
-                imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
+                imageSrc: "./img/paymenticons/Triodos Bank.png"
             }
         ];
 
