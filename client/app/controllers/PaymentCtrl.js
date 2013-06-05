ff362739e65fea235e67f2fba938240a28d0855f
Patrick van Vuuren <PMH.vanVuuren@student.han.nl>
2013-06-05 11:32:40 +0200

    dropdown plugin voor img in option

--- client/app/controllers/PaymentCtrl.js
+++ client/app/controllers/PaymentCtrl.js
@@ -1,3 +1,59 @@
 app.controller('PaymentCtrl', function($scope, $location, $http, $resource) {
+    $scope.addImageDropDowns = function() {
 
+        var paymentMethods = [
+            {
+                text: "iDeal",
+                value: 1,
+                selected: true,
+                description: "Betalen met uw eigen bank",
+                imageSrc: "http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png"
+            },
+            {
+                text: "Mastercard",
+                value: 2,
+                selected: false,
+                description: "Betalen met Mastercard",
+                imageSrc: "http://dl.dropbox.com/u/40036711/Images/twitter-icon-32.png"
+            },
+            {
+                text: "Paypal",
+                value: 3,
+                selected: false,
+                description: "Betaal met uw Paypal-account",
+                imageSrc: "http://dl.dropbox.com/u/40036711/Images/linkedin-icon-32.png"
+            },
+            {
+                text: "Visa",
+                value: 4,
+                selected: false,
+                description: "Betalen met Visa",
+                imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
+            },
+            {
+                text: "American Express",
+                value: 5,
+                selected: false,
+                description: "Betaal met American Express",
+                imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
+            },
+            {
+                text: "Discover Network",
+                value: 6,
+                selected: false,
+                description: "Betaal met het Discover Network",
+                imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
+            }];
+
+        $('#paymentMethods').ddslick({
+            data:paymentMethods,
+            width:300,
+            selectText: "Selecteer een betaal methode",
+            imagePosition:"left",
+            onSelected: function(selectedData){
+
+            }
+        });
+
+    }
 });
--- client/js/main.js
+++ client/js/main.js
@@ -1,34 +1,34 @@
 function EventController() {
-        this.addEvents = function() {
-            this.addSlideEvents();
+    this.addEvents = function() {
+        this.addSlideEvents();
+    };
+
+    this.addSlideEvents = function() {
+        this.addNavigationEvents();
+    };
+
+    this.addNavigationEvents = function () {
+        this.showLoginForm();
+    };
+
+    this.showLoginForm = function() {
+        var inlogNav = document.getElementById("inloggen");
+        inlogNav.onclick = function() {
+            show();
         };
 
-        this.addSlideEvents = function() {
-            this.addNavigationEvents();
+        var show = function() {
+            $("#loginContainer").slideDown();
+            inlogNav.onclick = function() {
+                hide();
+            };
         };
 
-        this.addNavigationEvents = function () {
-            this.showLoginForm();
-        };
-
-        this.showLoginForm = function() {
-            var inlogNav = document.getElementById("inloggen");
+        var hide = function() {
+            $("#loginContainer").slideUp();
             inlogNav.onclick = function() {
                 show();
             };
-
-            var show = function() {
-                $("#loginContainer").slideDown();
-                inlogNav.onclick = function() {
-                    hide();
-                };
-            };
-
-            var hide = function() {
-                $("#loginContainer").slideUp();
-                inlogNav.onclick = function() {
-                    show();
-                };
-            };
         };
+    };
 }
