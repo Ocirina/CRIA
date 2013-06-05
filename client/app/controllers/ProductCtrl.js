18003583b4c32e3974d047adabc1617f3a0e3439
Willem van Lent <wvlent@deactro.nl>
2013-06-05 14:38:27 +0200

    Product controller

--- client/app/controllers/ProductCtrl.js
+++ client/app/controllers/ProductCtrl.js
@@ -18,19 +18,23 @@ app.controller('ProductCtrl', function($scope, $location, $http, $resource, $rou
 
         var product = Product.get(function(data){
             $scope.product = data.result;
-            $scope.getSociable($scope.product._id, 'ratings');
+            $scope.getSociable($scope.product._id, 'ratings', $scope.createStarsFromNumber);
             $scope.getSociable($scope.product._id, 'comments');
         });
     };
 
-    $scope.getSociable = function(id, type) {
+    $scope.getSociable = function(id, type, callback) {
+        if(typeof callback !== "function") {
+            var callback = function(data) {
+                $scope[type] = data.result;
+            };
+        }
+
         var Item = $resource('http://autobay.tezzt.nl\\:43083/casedesign/' + id + '/' + type,{},
             {charge: {method:'GET', params:{charge:true}}}
         );
 
-        Item.get(function(data) {
-            $scope[type] = data.result;
-        });
+        Item.get(callback);
     };
 
     $scope.addProductToShopCart = function() {
@@ -47,7 +51,7 @@ app.controller('ProductCtrl', function($scope, $location, $http, $resource, $rou
         order = JSON.parse(window.localStorage['Order']);
         var productAdded = $scope.checkProductInShopCart($scope.product._id);
         if(!productAdded) {
-            order.orderlines.push({"product": $scope.product, "aantal":1});
+            order.orderlines.push({"caseDesign": $scope.product, "aantal":1});
             window.localStorage['Order'] = JSON.stringify(order);
         }
 
@@ -57,7 +61,7 @@ app.controller('ProductCtrl', function($scope, $location, $http, $resource, $rou
     $scope.checkProductInShopCart = function(productId) {
         var order = JSON.parse(window.localStorage['Order']);
         for(var i = 0; i < order.orderlines.length; i++) {
-            if(order.orderlines[i]["product"]._id == productId) {
+            if(order.orderlines[i]["caseDesign"]._id == productId) {
                 order.orderlines[i]["aantal"] += 1;
                 window.localStorage['Order'] = JSON.stringify(order);
                 return true;
@@ -65,4 +69,18 @@ app.controller('ProductCtrl', function($scope, $location, $http, $resource, $rou
         }
         return false;
     }
+
+    $scope.createStarsFromNumber = function(data) {
+        var ratings = [];
+
+        for(var i=0; i < 5; i++){
+            if(i < data.result){
+                ratings.push('icon-star');
+            } else {
+                ratings.push('icon-star-empty');
+            }
+        }
+
+        $scope.ratings = ratings;
+    };
 });
