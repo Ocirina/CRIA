73b869e61d176b2f926e05a1082a106cb6803480
Brian van Burken <brianburkertje@hotmail.com>
2013-06-05 12:47:56 +0200

    Limitations added to amount rating to be between 1 and 5.

--- server/app/models/rating.js
+++ server/app/models/rating.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 // Schema definition for Rating.
 var Rating = Schema({
-    amount:       {type: Number, required: true},
+    amount:       {type: Number, required: true, min: 1, max: 5},
     user:         {type: Schema.Types.ObjectId, ref: 'User', required: true, unique: true},
     caseDesign:   {type: Schema.Types.ObjectId, ref: 'CaseDesign', required: true, unique: true}
 });
