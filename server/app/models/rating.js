e6a9ef1d8213e6063e2cbc394d25d842ef9e56d7
Brian van Burken <brianburkertje@hotmail.com>
2013-06-05 11:59:37 +0200

    Made the uniques of user and casedesign to catch only 1 rating.

--- server/app/models/rating.js
+++ server/app/models/rating.js
@@ -5,8 +5,8 @@ var Schema = mongoose.Schema;
 // Schema definition for Rating.
 var Rating = Schema({
     amount:       {type: Number, required: true},
-    user:         {type: Schema.Types.ObjectId, ref: 'User', required: true},
-    caseDesign:   {type: Schema.Types.ObjectId, ref: 'CaseDesign', required: true}
+    user:         {type: Schema.Types.ObjectId, ref: 'User', required: true, unique: true},
+    caseDesign:   {type: Schema.Types.ObjectId, ref: 'CaseDesign', required: true, unique: true}
 });
 
 // Define the Mongoose model.
