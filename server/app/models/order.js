598041acf10047edebd5e341873364810a4985e7
Brian van Burken <brianburkertje@hotmail.com>
2013-06-05 10:20:52 +0200

    Unwanted properties removed.

--- server/app/models/order.js
+++ server/app/models/order.js
@@ -4,11 +4,13 @@ var Schema = mongoose.Schema;
 
 // Schema definition for Oder.
 var Order = Schema({
-    id:         {type: Number, required: true, index: true, unique: true},
-    name:       {type: String, required: true, index: true},
-    user:       {type: Schema.Types.ObjectId, ref: 'User', required: true},
-    address:    {type: Schema.Types.ObjectId, ref: 'Address', required: true},
-    orderState: {type: Schema.Types.ObjectId, ref: 'OrderState', required: true}
+    user:        {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    orderlines:  [{type: Schema.Types.ObjectId, ref: 'OrderLine'}],
+    orderState:  {type: Schema.Types.ObjectId, ref: 'OrderState'}
+});
+
+Order.pre('save', function(next, req, order, callback){
+  var orderlines = req.body.order.orderlines;
 });
 
 // Define the Mongoose model.
