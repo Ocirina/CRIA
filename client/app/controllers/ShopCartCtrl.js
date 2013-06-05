cc7685ec384f057be6681dc5be5c45cad8983f15
Willem van Lent <wvlent@deactro.nl>
2013-06-05 11:48:43 +0200

    Added remove to shoppingcart.

--- client/app/controllers/ShopCartCtrl.js
+++ client/app/controllers/ShopCartCtrl.js
@@ -19,7 +19,7 @@ app.controller('ShopCartCtrl', function($scope, $location, $http, $resource) {
             for(var i = 0; i < $scope.shopCartDesigns.length; i++) {
                 amount = $scope.shopCartDesigns[i]["aantal"];
 
-                if($scope.shopCartDesigns[i].product._id == product._id){
+                if($scope.shopCartDesigns[i].product._id === product._id){
                     amount = input.orderline.aantal;
                 }
 
@@ -28,7 +28,26 @@ app.controller('ShopCartCtrl', function($scope, $location, $http, $resource) {
 
             window.localStorage['shopCartDesigns'] = JSON.stringify(orderlines);
         }
-    }
+    };
+
+    $scope.removeProduct = function(productId) {
+        if(window.localStorage['shopCartDesigns']) {
+            $scope.shopCartDesigns = JSON.parse(window.localStorage['shopCartDesigns']);
+
+            console.log($scope.shopCartDesigns);
+
+            for(var i = 0; i < $scope.shopCartDesigns.length; i++) {
+                if(productId === $scope.shopCartDesigns[i].product._id){
+                    //$scope.shopCartDesigns[i];
+                    $scope.shopCartDesigns.splice(i, 1);
+                }
+            }
+
+            console.log($scope.shopCartDesigns);
+
+            window.localStorage['shopCartDesigns'] = JSON.stringify($scope.shopCartDesigns);
+        }
+    };
 
     $scope.loadPaymentMethods = function() {
         $location.path('/betalen/kiezen');
