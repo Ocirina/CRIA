d8af36c04cccd9254bf64d6c9bf6f9daf3884696
Brian van Burken <brianburkertje@hotmail.com>
2013-06-06 14:17:51 +0200

    Ensures that a person can only rate a design once.

--- server/app/models/rating.js
+++ server/app/models/rating.js
@@ -5,9 +5,12 @@ var Schema = mongoose.Schema;
 // Schema definition for Rating.
 var Rating = Schema({
     amount:       {type: Number, required: true, min: 1, max: 5},
-    user:         {type: Schema.Types.ObjectId, ref: 'User', required: true, unique: true},
-    caseDesign:   {type: Schema.Types.ObjectId, ref: 'CaseDesign', required: true, unique: true}
+    user:         {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    caseDesign:   {type: Schema.Types.ObjectId, ref: 'CaseDesign', required: true}
 });
+
+// Make sure it only has 1 rating per design.
+Rating.index({user: 1, caseDesign: 1}, {unique: true});
 
 // Define the Mongoose model.
 mongoose.model("Rating", Rating, "Ratings");
