0fc3b84009c3732079c507f0628b7f95a26be0e2
Willem van Lent <wvlent@deactro.nl>
2013-06-06 09:40:42 +0200

    Added comment to the product  controller

--- client/app/controllers/ProductCtrl.js
+++ client/app/controllers/ProductCtrl.js
@@ -1,4 +1,7 @@
 app.controller('ProductCtrl', function($scope, $location, $http, $resource, $routeParams) {
+    /**
+     * This function will get all the products by get request and put them in the $scope.products var.
+     */
     $scope.getProducts = function() {
         var Products = $resource('http://autobay.tezzt.nl\\:43083/casedesigns',{},
             {charge: {method:'GET', params:{charge:true}}}
@@ -9,6 +12,11 @@ app.controller('ProductCtrl', function($scope, $location, $http, $resource, $rou
 		});
     };
 
+    /**
+     * This function will put the product in the $scope.product variable. The id will be get out of the URL.
+     * Also it will get the comments and ratings. The ratings will be converted to an array with CSS classes.
+     * So if we loop trough the array we can use the classes to generate stars filled in or not.
+     */
     $scope.getProduct = function() {
         var id = $routeParams.id;
 
@@ -23,6 +31,14 @@ app.controller('ProductCtrl', function($scope, $location, $http, $resource, $rou
         });
     };
 
+    /**
+     * This function will make an get call to casedesigns. We will provide a param as type.
+     * If we put in "comments" as type it will get all the comments.
+     *
+     * @param {Number} id
+     * @param {String} type
+     * @param {Function} callback
+     */
     $scope.getSociable = function(id, type, callback) {
         if(typeof callback !== "function") {
             var callback = function(data) {
@@ -37,6 +53,9 @@ app.controller('ProductCtrl', function($scope, $location, $http, $resource, $rou
         Item.get(callback);
     };
 
+    /**
+     * This function will add the product to the local storage cart. This product will be provided by the $scope var.
+     */
     $scope.addProductToShopCart = function() {
         var order = {
             user: "51a7be85315f97dc39000001",
@@ -57,6 +76,13 @@ app.controller('ProductCtrl', function($scope, $location, $http, $resource, $rou
         $location.path('/winkelwagen');
     };
 
+    /**
+     * This function will check if the product is already in the cart. If it is there will be added 1 case.
+     * And true is returend so the product would not be added again as new product to the cart.
+     *
+     * @param {Number} productId
+     * @returns {boolean}
+     */
     $scope.checkProductInShopCart = function(productId) {
         var order = JSON.parse(window.localStorage['Order']);
         for(var i = 0; i < order.orderlines.length; i++) {
@@ -69,6 +95,11 @@ app.controller('ProductCtrl', function($scope, $location, $http, $resource, $rou
         return false;
     };
 
+    /**
+     * This function wil generate an array of Strings. These Strings are css classes. As the name says this are stars.
+     *
+     * @param {Array} data
+     */
     $scope.createStarsFromNumber = function(data) {
         var ratings = [];
 
