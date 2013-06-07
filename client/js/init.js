daebda358a8e9086c6ac263fb7b21bc154a778b9
Willem van Lent <wvlent@deactro.nl>
2013-06-07 10:12:38 +0200

    Added notifier, with jQuery (Sorry Theo ;-) )

--- client/js/init.js
+++ client/js/init.js
@@ -5,10 +5,13 @@
 
 
 var Application = function() {
-    this.notify = function(type, message) {
-        var warning = '<div class="notification-' + type + '">' + message + '</div>';
+    Application.notify = function(type, message) {
+        var warning = '<div style="display: none;" class="notification-' + type + '">' + message + '</div>';
+        $("#notifier").children().remove();
         $("#notifier").append(warning);
-    }
+        $("#notifier div").fadeIn(500);
+        $("#notifier div").fadeIn(500).delay(3000).fadeOut(500);
+    };
 };
 
 var application = new Application();
