68c0ab65bed009831449e808e0919fa067cd21e9
Brian van Burken <brianburkertje@hotmail.com>
2013-06-07 14:59:09 +0200

    Added basicDesign and fixed the preview.

--- server/app/models/case_design.js
+++ server/app/models/case_design.js
@@ -7,7 +7,8 @@ var CaseDesign = Schema({
     name:           {type: String,  required: true, index: true},
     shared:         {type: Boolean, required: true},
     user:           {type: Schema.Types.ObjectId, ref: 'User'},
-    preview:        {type: Schema.Types.ObjectId, ref: 'CaseDesign'},
+    preview:        {type: String, required: true},
+    basicDesign:    {type: Schema.Types.ObjectId, ref: 'CaseDesign'},
     caseModel:      {type: Schema.Types.ObjectId, ref: 'CaseModel'},
     designObjects:  [{type: Schema.Types.ObjectId, ref: 'DesignObject'}],
     created_at:     {type: Date, required: true, default: Date.now}
