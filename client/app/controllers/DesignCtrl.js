c7d77c363f65a3ac31ea2a6806400b2e9ae7f3af
Willem van Lent <wvlent@deactro.nl>
2013-06-12 10:35:49 +0200

    added array of cases.

--- client/app/controllers/DesignCtrl.js
+++ client/app/controllers/DesignCtrl.js
@@ -8,6 +8,11 @@ app.controller('DesignCtrl', function($scope, $location, $http, $resource, $rout
         $scope.phone.phone = phoneId;
 
         $.event.trigger('StartEditor');
+
+        $scope.backgroundsRowOne = [];
+        $scope.backgroundsRowTwo = {};
+
+        $scope.fillBackgrounds();
     };
 
     /**
@@ -21,9 +26,9 @@ app.controller('DesignCtrl', function($scope, $location, $http, $resource, $rout
 
     $scope.getCanvasDesign = function() {
         if(window.sessionStorage["loggedInUser"]) {
-            var caseId = $routeParams.id;
+            var caseId = $routeParams.id,
 
-            var Design = $resource('http://autobay.tezzt.nl\\:43083/casedesign/' + caseId,{},
+                Design = $resource('http://autobay.tezzt.nl\\:43083/casedesign/' + caseId,{},
                 {charge: {method:'GET', params:{charge:true}}}
             );
 
@@ -36,4 +41,132 @@ app.controller('DesignCtrl', function($scope, $location, $http, $resource, $rout
             // niet ingelogd
         }
     }
+
+    $scope.fillBackgrounds = function() {
+        $scope.backgroundsRowOne = [
+            {
+                image:  "img/patterns/dogs.png",
+                alt:    "Dogs"
+            },
+            {
+                image:  "img/patterns/dogs&pussies.png",
+                alt:    "Dogs and cats"
+            },
+            {
+                image:  "img/patterns/dots_1.png",
+                alt:    "Dots"
+            },
+            {
+                image:  "img/patterns/paarstreepjesG.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern1.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern2.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern3.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern4.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern5.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern6.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern7.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern8.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern9.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pattern10.png",
+                alt:    "Stripes"
+            },
+            {
+                image:  "img/patterns/pittig_pattern.png",
+                alt:    "Stripes"
+            }
+        ];
+
+        $scope.backgroundsRowTwo = [
+            {
+                image:  "img/patterns/skull.png",
+                alt:    "Dogs"
+            },
+            {
+                image:  "img/patterns/VF_pattern08.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/VF_pattern09.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/VF_pattern10.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/VF_pattern11.jpg",
+                alt:    "Dogs"
+            },
+            {
+                image:  "img/patterns/VF_pattern12.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/VF_pattern13.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/VF_pattern14.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/VF_pattern15.jpg",
+                alt:    "Dogs"
+            },
+            {
+                image:  "img/patterns/VF_pattern16.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/VF_pattern17.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/VF_pattern18.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/VF_pattern19.jpg",
+                alt:    "Dogs"
+            },
+            {
+                image:  "img/patterns/VF_pattern20.jpg",
+                alt:    "Skull"
+            },
+            {
+                image:  "img/patterns/winterswag.png",
+                alt:    "Skull"
+            }
+        ];
+    };
 });
