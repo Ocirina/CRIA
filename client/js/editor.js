6ee5116f95a8451f8e5499db318436b380a85569
Willem van Lent <wvlent@deactro.nl>
2013-06-13 11:16:25 +0200

    Bug fixes.

--- client/js/editor.js
+++ client/js/editor.js
@@ -1,70 +1,73 @@
 var dimensions = {
-  "4": {
-    width: 270,
-    height: 532
-  },
-  "5": {
-    width: 270,
-    height: 569
-  }
+    "4": {
+        width: 270,
+        height: 532
+    },
+    "5": {
+        width: 270,
+        height: 569
+    }
 };
 var canvas = null;
 
-window.onbeforeunload = function(e) {
-  canvas = null;
-  var elem = $(document.body);
-  console.log(elem);
-  if (elem.hasClass('case-editor')) {
-    elem.removeClass('case-editor');
-    return 'Weet u zeker dat u de pagina wilt verlaten zonder op te slaan?';
-  }
+window.onbeforeunload = function (e) {
+    canvas = null;
+    var elem = $(document.body);
+    if (elem.hasClass('case-editor')) {
+        elem.removeClass('case-editor');
+        return 'Weet u zeker dat u de pagina wilt verlaten zonder op te slaan?';
+    }
 };
 
 $(document).on('StartEditor', function (e, data) {
-  $(document.body).addClass('case-editor');
-  
-  if (canvas === null) {
-    canvas = new fabric.Canvas('case-editor', {
-      centerTransform: true,
-      controlsAboveOverlay: true,
-      selectionBorderColor: "black",
-      selectionColor: "rgba(255, 255, 255, 0.3)",
-      selectionLineWidth: 2
-    });
-  }
-  
-  if (canvas) {
-    var id = data.phone;
-    canvas.setWidth(dimensions[id].width);
-    canvas.setHeight(dimensions[id].height);
-    canvas.renderAll();
-  }
+    $(document.body).addClass('case-editor');
 
-  var Handler = {
-      handleTextObject: function(obj) {
-      $('#text').find('button').attr('disabled', false);
-          $('#text textarea').val(obj.getText());
-      },
-      handleImageObject: function(obj) {
-          console.log(obj);
-      },
-      handlePathObject: function(obj) {
-          console.log(obj);
-      },
-      handleGroupObject: function(obj) {
-          console.log(obj);
-      },
-      handleDefaultObject: function(obj) {
-          console.log(obj);
-      }
-  };
-    
-    canvas.on('object:selected', function(e){
+    if (canvas === null) {
+        canvas = new fabric.Canvas('case-editor', {
+            centerTransform: true,
+            controlsAboveOverlay: true,
+            selectionBorderColor: "black",
+            selectionColor: "rgba(255, 255, 255, 0.3)",
+            selectionLineWidth: 2
+        });
+    }
+
+    if (canvas) {
+        var id = data.phone;
+        canvas.setWidth(dimensions[id].width);
+        canvas.setHeight(dimensions[id].height);
+        canvas.renderAll();
+    }
+
+    var Handler = {
+        handleTextObject: function (obj) {
+            $('#text').find('button').attr('disabled', false);
+            $('#text textarea').val(obj.getText());
+        },
+        handleImageObject: function (obj) {
+            console.log(obj);
+        },
+        handlePathObject: function (obj) {
+            console.log(obj);
+        },
+        handleGroupObject: function (obj) {
+            console.log(obj);
+        },
+        handleDefaultObject: function (obj) {
+            console.log(obj);
+        }
+    };
+
+    canvas.on('object:selected', function (e) {
         var selectedObject = e.target,
             type = selectedObject.type,
-            fn = 'handle'+capitaliseFirstLetter(type)+'Object';
-        try { Handler[fn](selectedObject); }
-        catch(err) { Handler.handleDefaultObject(selectedObject); }
+            fn = 'handle' + capitaliseFirstLetter(type) + 'Object';
+        try {
+            Handler[fn](selectedObject);
+        }
+        catch (err) {
+            Handler.handleDefaultObject(selectedObject);
+        }
     });
 
     if (hasFileUploadSupport()) {
@@ -77,32 +80,32 @@ $(document).on('StartEditor', function (e, data) {
         //$('.phone').on({"dragover": stopEvent, "drop": handleDroppedFiles});
     }
 
-    $('.sel-bg').on('click', '.icon-caret-right', function(e){
-        move('.sel-bg .sliderow', '-=300');
+    $('.sel-bg').on('click', '.icon-caret-right', function (e) {
+        move('.sel-bg .sliderow', '-=250');
 
         e.preventDefault();
         e.stopPropagation();
         return false;
     });
 
-    $('.sel-bg').on('click', '.icon-caret-left', function(e){
-        move('.sel-bg .sliderow', '+=300');
+    $('.sel-bg').on('click', '.icon-caret-left', function (e) {
+        move('.sel-bg .sliderow', '+=250');
 
         e.preventDefault();
         e.stopPropagation();
         return false;
     });
 
-    $('.sel-object').on('click', '.icon-caret-right', function(e){
-        move('.sel-object .sliderow', '-=120');
+    $('.sel-object').on('click', '.icon-caret-right', function (e) {
+        move('.sel-object .sliderow', '-=150');
 
         e.preventDefault();
         e.stopPropagation();
         return false;
     });
 
-    $('.sel-object').on('click', '.icon-caret-left', function(e){
-        move('.sel-object .sliderow', '+=120');
+    $('.sel-object').on('click', '.icon-caret-left', function (e) {
+        move('.sel-object .sliderow', '+=150');
 
         e.preventDefault();
         e.stopPropagation();
@@ -112,125 +115,106 @@ $(document).on('StartEditor', function (e, data) {
     function move(el, value) {
         $(el).animate({
             left: value,
-        }, 500, function() {
+        }, 500, function () {
         });
     }
 
     $('#add-text').on('click', function (e) {
         var settings = {
-            originX:        'left',
-            lineHeight:     1,
-            fontFamily:     $('#set-font').val().toLowerCase(),
-            fontSize:       +$('#set-font-size').val(),
-            fill:           $('#set-font-color').val(),
-            fontWeight:     'normal'
-         };
+            originX: 'left',
+            lineHeight: 1,
+            fontFamily: $('#set-font').val().toLowerCase(),
+            fontSize: +$('#set-font-size').val(),
+            fill: $('#set-font-color').val(),
+            fontWeight: 'normal'
+        };
 
         addText($('#set-text').val(), settings);
         $('#set-text').val('');
         return stopEvent(e);
     });
-    
-    $('.sel-bg').find('.background-slider').on('click', 'img', function(e){
-      var src = e.target.src;
-      setBackground(src);
+
+    $('.sel-bg').find('.background-slider').on('click', 'img', function (e) {
+        var src = e.target.src;
+        setBackground(src);
     });
 
-    $('.sel-object').find('.background-slider').on('click', 'img', function(e){
+    $('.sel-object').find('.background-slider').on('click', 'img', function (e) {
         var src = e.target.src;
         addSvg(src);
     });
 
-    $('.form-horizontal').on('click', 'button', function(e){
-<<<<<<< HEAD
+    $('.form-horizontal').on('click', 'button', function (e) {
         if (window.sessionStorage["loggedInUser"]) {
-        try {
-          canvas.deactivateAll().renderAll();
-          var img = canvas.toDataURL('png'),
-          json = JSON.stringify(canvas),
-          data = {},
-          user = JSON.parse(window.sessionStorage["loggedInUser"]);
-        
-          data.name = $('input#name').val();
-          data.preview = img;
-          data.canvas = json;
-          data.shared = true;
-          data.user = user._id;
-        
-          $.ajax({
-            url: 'http://autobay.tezzt.nl:43083/casedesigns',
-            type: 'POST',
-            data: data,
-            success: function(response) {
-              console.log(response);
-=======
-        if(window.sessionStorage["loggedInUser"]) {
             try {
+                canvas.deactivateAll().renderAll();
+                var img = canvas.toDataURL('png'),
+                    json = JSON.stringify(canvas),
+                    data = {},
+                    user = JSON.parse(window.sessionStorage["loggedInUser"]);
 
-              var img = canvas.toDataURL('png'),
-              json = JSON.stringify(canvas),
-              data = {},
-              user = JSON.parse(window.sessionStorage["loggedInUser"]);
+                data.name = $('input#name').val();
+                data.preview = img;
+                data.canvas = json;
+                data.shared = true;
+                data.user = user._id;
 
-              data.name = $('input#name').val();
-              data.preview = img;
-              data.canvas = json;
-              data.shared = true;
-              data.user = user._id;
-
-              $.ajax({
-                url: 'http://autobay.tezzt.nl:43083/casedesigns',
-                type: 'POST',
-                data: data,
-                success: function(response) {
-                  console.log(response);
-                    Application.notify('ok', 'Je hoesje is opgeslagen!');
-                }
-              });
-              console.log(json);
->>>>>>> 51c45b3133d37318225351c2ca8dea98ff043f43
+                $.ajax({
+                    url: 'http://autobay.tezzt.nl:43083/casedesigns',
+                    type: 'POST',
+                    data: data,
+                    success: function (response) {
+                        console.log(response);
+                    }
+                });
             }
-            catch(e) {
-              Application.notify('error', 'Je browser ondersteund geen export van de canvas, helaas!');
+            catch (e) {
+                Application.notify('error', 'Je browser ondersteund geen export van de canvas, helaas!');
             }
-      } else {
+        } else {
             Application.notify('error', 'Je moet ingelogd zijn om een ontwerp te kunnen opslaan.');
+
         }
-      e.preventDefault();
-      e.stopPropagation();
-      return false;
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
     });
 
     function addSvg(svg) {
-      fabric.loadSVGFromURL(svg, function(objects, options) {
-        var loadedObject = fabric.util.groupSVGElements(objects, options);
-        var offset = 50,
-            left = fabric.util.getRandomInt(0 + offset, 270 - offset),
-            top = fabric.util.getRandomInt(0 + offset, 650 - offset),
-            angle = fabric.util.getRandomInt(-20, 40);
-        loadedObject.set({
-          left: left,
-          top: top,
-          angle: angle,
-          padding: 10,
-          cornersize: 10
+        fabric.loadSVGFromURL(svg, function (objects, options) {
+            var loadedObject = fabric.util.groupSVGElements(objects, options);
+            var offset = 50,
+                left = fabric.util.getRandomInt(0 + offset, 270 - offset),
+                top = fabric.util.getRandomInt(0 + offset, 650 - offset),
+                angle = fabric.util.getRandomInt(-20, 40);
+            loadedObject.set({
+                left: left,
+                top: top,
+                angle: angle,
+                padding: 10,
+                cornersize: 10
+            });
+            canvas.add(loadedObject);
         });
-        canvas.add(loadedObject);
-      });
     }
-    
+
     function setBackground(background, hex) {
-      var setting = background;
-      if (hex === null) { hex = false; }
-      if (!hex) { setting = { source: background, repeat: 'repeat' }; }
-      canvas.setBackgroundColor(setting, function () { 
-        canvas.renderAll(); 
-      });
+        var setting = background;
+        if (hex === null) {
+            hex = false;
+        }
+        if (!hex) {
+            setting = { source: background, repeat: 'repeat' };
+        }
+        canvas.setBackgroundColor(setting, function () {
+            canvas.renderAll();
+        });
     }
-    
+
     function capitaliseFirstLetter(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
+
     function handleDroppedFiles(e) {
         console.log(e);
         //addFiles(e.dataTransfer.files, canvas);
@@ -263,7 +247,9 @@ $(document).on('StartEditor', function (e, data) {
     function addFiles(files) {
         var reader = null;
         for (var i = 0, f; f = files[i]; i++) {
-            if (!f.type.match('image.*')) { continue; }
+            if (!f.type.match('image.*')) {
+                continue;
+            }
             reader = new FileReader();
             reader.onload = (function (theFile) {
                 return function (e) {
@@ -290,7 +276,7 @@ $(document).on('StartEditor', function (e, data) {
     }
 
     function addImageToCanvas(data) {
-      console.log(data);
+        console.log(data);
         fabric.Image.fromURL(data, function (obj) {
             var ratio = calculateRatio(data, 270, 572);
             var settings = calculateCenter(270, 572);
