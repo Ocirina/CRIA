2c74ff577d3d642997783feaf92482617442a574
Willem van Lent <wvlent@deactro.nl>
2013-06-18 12:31:08 +0200

    jslint

--- client/js/util.js
+++ client/js/util.js
@@ -5,23 +5,40 @@
  * @return {Object} True if empty else false.
  */
 function isEmpty(obj) {
-    if (typeof obj === 'undefined' || obj === null || obj === '') { return true; }
-    if (typeof obj === 'number' && isNaN(obj)) { return true; }
-    if (obj.length === 0) { return true; }
-    if (obj instanceof Date && isNaN(Number(obj))) { return true; }
+    "use strict";
+
+    if (obj === 'undefined' || obj === null || obj === '') {
+        return true;
+    }
+    if (typeof obj === 'number' && isNaN(obj)) {
+        return true;
+    }
+    if (obj.length === 0) {
+        return true;
+    }
+    if (obj instanceof Date && isNaN(Number(obj))) {
+        return true;
+    }
     return false;
 }
 
 function stopEvent(e) {
+    "use strict";
+
     e.stopPropagation();
     e.preventDefault();
+
     return false;
 }
 
 function capitaliseFirstLetter(string) {
+    "use strict";
+
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function hasFileUploadSupport() {
+    "use strict";
+
     return (window.File && window.FileReader && window.FileList && window.Blob);
 }
